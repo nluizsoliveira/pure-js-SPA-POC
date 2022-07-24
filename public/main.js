d205db869ab097e626c60cab786551dfc7614b7a
nluizsoliveira <nluizsoliveira@gmail.com>
2022-07-24 04:04:32 -0300

    feat(dynamic-page): split contents in module

--- public/main.js
+++ public/main.js
@@ -1,27 +1,38 @@
-import {PROJECTS_CONTENT} from '/pages_content/contents.js'
+import {CONTENTS} from '/pages_content/contents.js'
 
+/* WHY EVENT LISTENERS INSTEAD OF <div onclick=function(this)> ?  
+*   main.js must be a module to import content.js module exported variables. 
+*   HTML cannot access module functions. Solutions are:
+*       1) binding functions to window object OR
+*       2) manually adding event listeners (chosen) 
+*/
 const NAVBAR_SELECTOR = "#navbar"
 const navBar = document.querySelector(NAVBAR_SELECTOR)
 for(const navItem of navBar.children){
-    navItem.addEventListener('click', (e)=>{
-        renderContent(e)
-    })
+    navItem.addEventListener("click", e => {renderContent(e.target)})
 }
 
-export const renderContent =  function(clickedNavItem){
-    const contentOptionId = clickedNavItem.target.id
-    activateNavbar(contentOptionId)
-    console.log(PROJECTS_CONTENT)
+const renderContent =  function(clickedNavItem){
+    const contentId = clickedNavItem.id
+    activateNavbar(contentId)
+    const content = CONTENTS[contentId]
+    renderTitle(content)
 }
 
-export const activateNavbar = function(optionId){
+const activateNavbar = function(contentId){
     const ACTIVE_ITEM_CLASS = "activeNavItem"
     const navBar = document.querySelector(NAVBAR_SELECTOR)
     for (const navItem of navBar.children){
-        if(navItem.id === optionId){
+        if(navItem.id === contentId){
             navItem.classList.add(ACTIVE_ITEM_CLASS)
         } else {
             navItem.classList.remove(ACTIVE_ITEM_CLASS)
         }
     }
 }
+/*
+const renderTitle = function(content){
+    const TITLE_ID = '#title'
+    const titleTag = document.querySelector(TITLE_ID)
+    titleTag.innerHTML = content[0].title
+}*/
--- public/pages_content/contents.js
+++ public/pages_content/contents.js
@@ -1,13 +1,15 @@
-const PROJECTS_CONTENT = [
-    {
-        "title": "Certificate Generator",
-        "firstParagraph": "Node.js batch script that parses a .csv file and generates .pdf certificates for each line/person",
-        "firstImgUrl": "images/certificate0.png",
-        "secondImgUrl": "images/certificate.png",
-        "projectUrl": "https://github.com/USPCodeLabSanca/Node-Batch-Certificate-Generator",
-        "secondParagraph": "Mentor of the project, responsible for idealizing, writing requirements, spliting requirements in features divided across team and coding",
-        "thirdParagraph": "Used in real life recurrently for generating USPCodelab courses, hackathons and other events certificates."
-    }
-]
+const CONTENTS = {
+    'projects': [
+        {
+            "title": "Certificate Generator",
+            "firstParagraph": "Node.js batch script that parses a .csv file and generates .pdf certificates for each line/person",
+            "firstImgUrl": "images/certificate0.png",
+            "secondImgUrl": "images/certificate.png",
+            "projectUrl": "https://github.com/USPCodeLabSanca/Node-Batch-Certificate-Generator",
+            "secondParagraph": "Mentor of the project, responsible for idealizing, writing requirements, spliting requirements in features divided across team and coding",
+            "thirdParagraph": "Used in real life recurrently for generating USPCodelab courses, hackathons and other events certificates."
+        },
+    ]
+}
 
-export {PROJECTS_CONTENT}
+export {CONTENTS}
