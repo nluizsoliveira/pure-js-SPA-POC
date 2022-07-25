bd75deb492fbee29fb9d50c4355d74dc84d04835
nluizsoliveira <nluizsoliveira@gmail.com>
2022-07-25 03:32:46 -0300

    feat(dynamic-page): rename imgs, add gitbook project

--- public/pages_content/contents.js
+++ public/pages_content/contents.js
@@ -3,8 +3,8 @@ const CONTENTS = {
         {    
             "title": "Certificate Generator",
             "firstParagraph": "Node.js batch script that parses a .csv file and generates .pdf certificates for each line/person",
-            "firstImgUrl": "pages_content/images/certificate0.png",
-            "secondImgUrl": "pages_content/images/certificate.png",
+            "firstImgUrl": "pages_content/images/project_1_1.png",
+            "secondImgUrl": "pages_content/images/project_1_2.png",
             "url": "https://github.com/USPCodeLabSanca/Node-Batch-Certificate-Generator",
             "secondParagraph": "Mentor of the project, responsible for idealizing, writing requirements, spliting requirements in features divided across team and coding",
             "thirdParagraph": "Used in real life recurrently for generating USPCodelab courses, hackathons and other events certificates."
@@ -12,11 +12,20 @@ const CONTENTS = {
         {    
             "title": "Frontend-API-Mysql POC",
             "firstParagraph": "Frontend/Backend website, implemented through an API/ middleware/ dbConnector pattern for a simple library system. ",
-            "firstImgUrl": "pages_content/images/project_2_2.png",
-            "secondImgUrl": "pages_content/images/project_2_1.png",
+            "firstImgUrl": "pages_content/images/project_2_1.png",
+            "secondImgUrl": "pages_content/images/project_2_2.png",
             "url": "https://github.com/nluizsoliveira/API-middleware-dbConnector-POC",
             "secondParagraph": "Backend API implemented in Python's Flask manipulates and retrieve data from a MySQL database.",
             "thirdParagraph": "Frontend implemented in pure HTML, CSS and Javascript allows creating, searching for and deleting books"
+        },
+        {    
+            "title": "Gitbook",
+            "firstParagraph": "e-book on Web Development available at <a class = 'highlight' href = 'https://uclsanca.gitbook.io'>uclsanca.gitbook.io</a>",
+            "firstImgUrl": "pages_content/images/project_3_1.png",
+            "secondImgUrl": "pages_content/images/project_3_2.png",
+            "url": "https://github.com/USPCodeLabSanca/gitbook",
+            "secondParagraph": "Managed the project, breaking syllabus in weekly delivers divided among team in a doing/wrote/reviewed system.",
+            "thirdParagraph": "Defined the sylbbus and wrote some of the Javascript chapters"
         }
     ]
 }
