692112d39e68fa230508a00f65c18f2a44cfd4cb
nluizsoliveira <nluizsoliveira@gmail.com>
2022-10-30 22:01:14 -0300

    fix(blog): fix nav styling

--- public/components/navbar/test/test.js
+++ public/components/navbar/test/test.js
@@ -1,4 +1,7 @@
-import {initialRenderNav} from '/components/navbar/controller.js'
+import {NAV_STATES} from '/components/navbar/model.js'
+import {renderNav} from '/components/navbar/controller.js'
+import {stateLifter} from '/components/index/controller.js'
 
 const root = document.getElementById("navRoot")
-initialRenderNav(root)
+renderNav(root, NAV_STATES.experienceList, stateLifter);
+
